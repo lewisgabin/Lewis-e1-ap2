137a7ffebb17c68cb2212ef4e13d79cc3befade6
lewisgabin <lewisgabin27@gmail.com>
2020-10-07 18:48:59 -0400

    Subiendo Migrations y base de datos

---
+++ Migrations/20201007224823_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lewis_e1_ap2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Stock = table.Column<int>(nullable: false),
+                    Costo = table.Column<decimal>(nullable: false),
+                    ValorInventario = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
